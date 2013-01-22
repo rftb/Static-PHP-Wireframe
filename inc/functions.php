f84f9045b7d7a84e6e62b2dd8cbb9cef46e8ea40
Preston <team@rideforthebrand.net>
2013-01-22 13:59:13 -0600

    isMobile

---
+++ inc/functions.php
@@ -0,0 +1,7 @@
+<?php 
+
+// Create the function, so you can use it
+function isMobile() {
+    return preg_match("/(android|avantgo|blackberry|bolt|boost|cricket|docomo|fone|hiptop|mini|mobi|palm|phone|pie|tablet|up\.browser|up\.link|webos|wos)/i", $_SERVER["HTTP_USER_AGENT"]);
+}
+
